4e82e358678894f5d59ad199b453a390612bd279
Erreal <erreal@yandex.ru>
2019-05-01 11:18:19 +0300

    Добавил стандарты ХТМЛ и подключил стили

--- index.php
+++ index.php
@@ -1,1 +1,17 @@
-Helo world
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="ru-RU">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="../css/styles.css"></head>
+
+<body>
+
+<?php
+
+
+?>
+
+</body>
+
+</html>
