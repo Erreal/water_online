f5afb38e8f6a77176dc96afaa66dbebc2cd063b7
Erreal <erreal@yandex.ru>
2019-05-02 14:00:12 +0300

    Сверстал первую секцию

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" lang="ru-RU">
-<head>
-	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<!DOCTYPE html>
+<html lang="ru">
+  <head>
+    <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Вода.ОНЛАЙН - доставка питьевой бутилированной воды на дом и в офис.</title>
-	<link rel="stylesheet" type="text/css" href="css/styles.css">
+    <link href="css/styles.css" rel="stylesheet">
     <link href="css/bootstrap.min.css" rel="stylesheet">
 </head>
 
@@ -14,13 +14,51 @@
 	<header class="header">
 		<img class="logo" src="/images/logo.png">
 		<div class="header-actions">
-			<p class="top-text">Звоните для заказа: <span>+7 (495) 664-97-47</span></p>
-			<p class="bottom-text">Заказ по электронной почте: <a href="mailto:info@вода.онлайн">info@вода.онлайн</a></p>
+			<p class="top-text hidden-xs">Звоните для заказа: <span>+7 (800) 000-00-00</span></p>
+			<p class="bottom-text hidden-xs">Заказ по электронной почте: <a href="mailto:info@вода.онлайн">info@вода.онлайн</a></p>
+			<p class="mid-text visible-xs hidden-sm hidden-md hidden-ld"><a href="tel:+78000000000">+7 (800) 000-00-00</a></p>
 		</div>
 	</header>
 	<section class="content1">
-		<div class="content-container">
-
+		<div class="container">
+			<div class="content-cancel hidden-xs hidden-sm"><img class="cancel-anytime-sticker" src="images/new-cancel-anytime-program.png"> </div>
+			<div class="content-text">
+				<h1>Получите скидку 20% на первый заказ!</h1>
+				<p>Закажите воду у нас, и наслаждайтесь доставкой до двери, в удобное для вас время.</p>
+			</div>
+		</div>
+		<div class="container">
+             <div class="col-md-8 col-md-8 col-md-push-4 content-offer">
+             	<div class="offer-block">
+             		<h2>Преимущества доставки бутилированной воды</h2>
+             		<p>
+             			<i class="fa fa-fw fa-check"></i> Удобная доставка
+             		</p>
+             		<p>
+             			<i class="fa fa-fw fa-check"></i> Приятный вкус
+             		</p>
+             		<p>
+             			<i class="fa fa-fw fa-check"></i> Строгий контроль качества
+             		</p>
+             		<p>
+             			<i class="fa fa-fw fa-check"></i> Горячая и холодная вода в любое время
+             		</p>
+             		<p>
+             			<i class="fa fa-fw fa-check"></i> От 200 рублей за 19 литровую бутыль
+             		</p>
+             		<p>
+             			<i class="fa fa-fw fa-check"></i> Большой ассортимент оборудования и аксессуаров
+             		</p>
+             	</div>
+             	<img class="bottles-coolers hidden-xs" src="images/bottles-coolers.png" />
+             </div>
+             <div class="content-city col-md-4 col-md-4 col-md-pull-8">
+                <div id="city" class="city">
+             		<h3>Выберите город</h3>
+             		<p>Пожалуйста выберите город, чтобы мы могли показать вам товары, доступные в вашей области.</p>
+             		<a onclick="submitSectionClick('1');"><input class="btn btn-lrg btn-success" type="button" value="Выбрать"></a>
+             	</div>
+             </div>
 		</div>
 	</section>
 	<section class="footer">
