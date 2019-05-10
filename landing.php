a601dc6cdea8c35d9b52db85a078a27dc37d712c
Erreal <erreal@yandex.ru>
2019-05-10 12:23:28 +0300

    Добавил проверку выбора города. Вынес посадочную страницу в файл landing.php

---
+++ landing.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html lang="ru">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Вода.ОНЛАЙН - доставка питьевой бутилированной воды на дом и в офис.</title>
+    <link href="css/styles.css" rel="stylesheet">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+
+</head>
+
+<body>
+<div class="main-contr">
+	<header class="header">
+		<img class="logo" src="/images/logo.png">
+		<div class="header-actions">
+			<p class="top-text hidden-xs">Звоните для заказа: <span>+7 (800) 000-00-00</span></p>
+			<p class="bottom-text hidden-xs">Заказ по электронной почте: <a href="mailto:info@вода.онлайн">info@вода.онлайн</a></p>
+			<p class="mid-text visible-xs hidden-sm hidden-md hidden-ld"><a href="tel:+78000000000">+7 (800) 000-00-00</a></p>
+		</div>
+	</header>
+	<section class="content1">
+		<div class="container">
+			<div class="content-cancel hidden-xs hidden-sm"><img class="cancel-anytime-sticker" src="images/new-cancel-anytime-program.png"> </div>
+			<div class="content-text">
+				<h1>Получите скидку* 20% на первый заказ!</h1>
+				<p>Закажите воду у нас, и наслаждайтесь доставкой до двери, в удобное для вас время.</p>
+			</div>
+		</div>
+		<div class="container">
+             <div class="col-md-8 col-md-8 col-md-push-4 content-offer">
+             	<div class="offer-block">
+             		<h2>Преимущества доставки бутилированной воды</h2>
+             		<p>
+             			<i class="fa fa-fw fa-check"></i> Удобная доставка
+             		</p>
+             		<p>
+             			<i class="fa fa-fw fa-check"></i> Приятный вкус
+             		</p>
+             		<p>
+             			<i class="fa fa-fw fa-check"></i> Строгий контроль качества
+             		</p>
+             		<p>
+             			<i class="fa fa-fw fa-check"></i> Горячая и холодная вода в любое время
+             		</p>
+             		<p>
+             			<i class="fa fa-fw fa-check"></i> От 200 рублей за 19 литровую бутыль
+             		</p>
+             		<p>
+             			<i class="fa fa-fw fa-check"></i> Большой ассортимент оборудования и аксессуаров
+             		</p>
+             	</div>
+             	<img class="bottles-coolers hidden-xs" src="images/bottles-coolers.png" />
+             </div>
+             <div class="content-city col-md-4 col-md-4 col-md-pull-8">
+                <div id="city" class="city">
+             		<h3>Выберите город</h3>
+             		<p>Пожалуйста выберите город, чтобы мы могли показать вам товары, доступные в вашей области.</p>
+             		<a onclick="submitSectionClick('1');"><input class="btn btn-lrg btn-success" type="button" value="Выбрать"></a>
+             	</div>
+             </div>
+		</div>
+		<div class="container">
+			<div class="footnote">
+				<p>*Предложение действительно при заказе от 3 бутылей воды и диспенсера. </p>
+			</div>
+		</div>
+	</section>
+	<section class="container-fluid how-it-works">
+		<div class="container">
+			<h2>Как это работает.</h2>
+			<div class="icons">
+				<div class="col-md-4 icon">
+					<img class="icon-img" src="images/twochecks.png">
+					<h3>Посмотрите<br /> наше предложение</h3>
+					<p>Широкий выбор бутилированной воды, расходных материалов и оборудования для напитков!</p>
+				</div>
+				<div class="col-md-4 icon">
+					<img class="icon-img" src="images/finger.png">
+					<h3>Выберите<br />понравившееся вам</h3>
+					<p>Используйте наш ассортимент продуктов и оборудования для выбора необходимых вам товаров.</p>
+				</div>
+				<div class="col-md-4 icon">
+					<img class="icon-img" src="images/truck.png">
+					<h3>Получите<br />ваш заказ</h3>
+					<p>Наш курьер привезет вамш заказ в удобное время, прямо до вашей двери.</p>
+				</div>
+			</div>
+		</div>
+	</section>
+	<section class="footer">
+	</section>
+</div>
+<?php
+
+
+?>
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+</body>
+
+</html>
